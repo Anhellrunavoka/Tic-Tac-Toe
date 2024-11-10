e0f84a420153a982973f1e519672164f42184b8c
Anhelina Gordieieva <angelina.gordeeva10@gmail.com>
2024-11-10 16:29:49 +0200

    Додали шаблони  ,різні змінні для наслідувальних класів і віртуальні функції

--- Tic-Tac-Toe.cpp
+++ Tic-Tac-Toe.cpp
@@ -12,40 +12,60 @@ public:
     Player(string nam, char sym, int level):name(nam), symbol(sym), level(level) {}
     string getName() const { return name; }
     char getSymbol() const { return symbol; }
-    virtual int getLVL() const { return level; }
-    void setALL(const string& nam, const char& sym, const int& lvl) {
+    int getLVL() const { return level; }
+    void SetALL(const string& nam, const char& sym, const int& lvl) {
         this->name = nam;
         this->symbol = sym;
         this->level = lvl;
     }
+    virtual string getPlayerData() const {
+        return name + ": " + symbol + ", Level: " + to_string(level);
+    }
     friend ostream& operator<<(ostream& output, const Player& play) {
-        output << play.getName() << ":" << " " << "," << 0;
+        output << play.getPlayerData();
         return output;
     }
 };
 class Player1 :public Player {
+    int score;
 public:
-    Player1():Player(" ",' ',0){}
-    Player1(string name,char sym, int level) :Player(name,sym,level){}
+    Player1():Player(" ",' ',0),score(0){}
+    void SetScore(const int& scr) { this->score= scr; }
+    int getScore() const { return score; }
+    Player1(string name,char sym, int level,int score) :Player(name,sym,level),score(score){}
+    virtual string getPlayerData() const {
+        return Player::getPlayerData() + ", Score: " + to_string(score);
+    }
     friend ostream& operator<<(ostream& output, const Player1& play) {
-        output << play.getName() << ":" << play.getSymbol() << "," << play.getLVL();
+        output << play.getPlayerData();
         return output;
     }
 };
 class Player2 :public Player {
+    int experience;
+    string y_d_m;
 public:
-    Player2() :Player(" ", ' ', 0){}
-    Player2(string name, char sym,int level) :Player(name, sym, level) {}
+    Player2() :Player(" ", ' ', 0), experience(0), y_d_m(" "){}
+    Player2(string name, char sym,int level,int exp) :Player(name, sym, level), experience(exp){}
+    void SetY_D_M(const string& ydm) { this->y_d_m = ydm; }
+    void SetExp(const int& exper) { this->experience = exper; }
+    int getExp() const { return experience; }
+    string getY_D_M() const { return y_d_m; }
+    virtual string getPlayerData() const{
+        return Player::getPlayerData() + ", Experience: " + to_string(experience) + " " + y_d_m;
+    }
     friend ostream& operator<<(ostream& output, const Player2& play) {
-        output << play.getName() << ":" << play.getSymbol() << "," << play.getLVL();
+        output << play.getPlayerData();
         return output;
     }
 };
 template <typename T, typename T2>
-void setPlayerDani(T& player, const string& name, const char& symbol, const int& level,
-    T2& player2, const string& name2, const char& symbol2, const int& level2) {
-    player.setALL(name, symbol, level);
-    player2.setALL(name2, symbol2, level2);
+void setPlayerDani(T& player1, const string& name, const char& symbol, const int& level, const int& score,
+    T2& player2, const string& name2, const char& symbol2, const int& level2,const int& exp) {
+    player1.SetALL(name, symbol, level);
+    player2.SetALL(name2, symbol2, level2);
+    player1.SetScore(score);
+    player2.SetExp(exp);
 }
 void print_pole(short krestik, short nolik, int pole[3][3]) {
     cout << "\t\tTic tac toe" << endl;
@@ -53,7 +73,6 @@ void print_pole(short krestik, short nolik, int pole[3][3]) {
     for (short i = 0; i < 3; i++) {
         cout << "\t\t";
         for (short j = 0; j < 3; j++) {
-            char symbol;
             cout << "|";
             (pole[i][j] == krestik) ? cout << "X" : (pole[i][j] == nolik) ? cout << "O" : cout << " ";
             cout << "| ";
@@ -108,7 +127,7 @@ class Game {
     Player1 player1;
     Player2 player2;
     string nam1, nam2;
-    int lev1, lev2;
+    int lev1, lev2,score,exper, yrdymth;
     short  krestik = -1, nolik = -1;
     int vibor, klitka,again, same,win=0;
     double time;
@@ -181,6 +200,10 @@ public:
                 cin >> nam1 >> nam2;
                 cout << "Input level first and second player(with space): " << endl;
                 cin >> lev1 >> lev2;
+                cout << "Input firts player wins and second player experience with(days(1),month(2),years(3)): " << endl;
+                cin >> score>>exper>> yrdymth;
+                if(yrdymth>3 && yrdymth<1){ throw invalid_argument("Input from 1 to 3"); }
+                (yrdymth == 1) ? player2.SetY_D_M("days") : (yrdymth == 2) ? player2.SetY_D_M("months") : player2.SetY_D_M("years");
                 (first_hid == 1) ? cout << "First hid 1 player" << endl : cout << "First hid 2 player" << endl;
                 cout << "Krestik(1) or nolik(2)? " << endl;
                 cin >> vibor;
@@ -205,11 +228,11 @@ public:
                         nolik = first_hid;
                     }
                 }
-                (first_hid == 1 && krestik == 1) ? setPlayerDani(player1, nam1, 'X', lev1, player2, nam2, 'O', lev2) :
-                    (first_hid == 1 && nolik == 1) ? setPlayerDani(player1, nam1, 'O', lev1, player2, nam2, 'X', lev2) :
-                    (first_hid == 2 && krestik == 2) ? setPlayerDani(player1, nam1, 'O', lev1, player2, nam2, 'X', lev2) :
-                    setPlayerDani(player1, nam1, 'X', lev1, player2, nam2, 'O', lev2);
-                cout << player1 << endl << player2 << endl;
+                (first_hid == 1 && krestik == 1) ? setPlayerDani(player1, nam1, 'X', lev1,score, player2, nam2, 'O', lev2,exper) :
+                    (first_hid == 1 && nolik == 1) ? setPlayerDani(player1, nam1, 'O', lev1, score, player2, nam2, 'X', lev2, exper) :
+                    (first_hid == 2 && krestik == 2) ? setPlayerDani(player1, nam1, 'O', lev1, score, player2, nam2, 'X', lev2, exper) :
+                    setPlayerDani(player1, nam1, 'X', lev1, score, player2, nam2, 'O', lev2, exper);
+                cout<<endl << "Dani 1 player: "<<endl<<player1 << endl <<"\nDani 2 player: " << endl << player2 << endl << endl;
                 do {
                     do {
                         print_pole(krestik, nolik, pole);
